e5c8baa6468fcad3b520581900f78cc1b8da08f2
Ardeshir <examples21@gmail.com>
2021-01-13 22:12:11 +0300

    edit

--- apps/baseApp/scrapers/Amazon.py
+++ apps/baseApp/scrapers/Amazon.py
@@ -6,7 +6,7 @@ class ProductPage():
     def __init__(self, product_link):
         try:
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0.1',
                 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Language' : 'en-US,en;q=0.9',
                 'pragma': 'no-cache',
